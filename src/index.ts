149936dda831571678b40d6d887b5c3579944592
Francesco Luigi Noceti <francescoluiginoceti@gmail.com>
2024-05-16 20:47:54 +0800

    docs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import compression from "compression";
 import routerV1 from "./router/v1";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
@@ -26,12 +27,20 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 app.listen(port as number, "0.0.0.0");
 
-app.get("/", (_req, res) =>
-  res.send(
-    "<div>Welcome to InvestConservation APIs! Contact <a href=`mailto:info@invetconservation.com`>info@invetconservation.com</a> for further details.</div>"
-  )
-);
+// app.get("/", (_req, res) =>
+//   res.send(
+//     "<div>Welcome to InvestConservation APIs! Contact <a href=`mailto:info@invetconservation.com`>info@invetconservation.com</a> for further details.</div>"
+//   )
+// );
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+var options = {
+  customCss: ".swagger-ui .topbar { display: none } body {padding: 0 10%;} ",
+  // customCssUrl:
+  //   "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  customSiteTitle: "InvestConservation API",
+  customfavIcon: "./favicon.ico",
+};
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 app.use("/v1", routerV1());
